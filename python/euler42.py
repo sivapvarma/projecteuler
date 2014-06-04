ed669a4db336b489baf49f061b2d296df6c5b3d1
Siva Prasad Varma <sivapvarma@gmail.com>
2014-06-04 17:07:54 +0530

    prob 42

---
+++ python/euler42.py
@@ -0,0 +1,15 @@
+from math import sqrt
+if __name__ == '__main__':
+    with open('words42.txt') as f:
+        words = f.read().strip('\n').split(',')
+    print(len(words))
+    words = [w.strip('"') for w in words]
+    cnt = 0
+    for w in words:
+        tmp = 0
+        for c in w:
+            tmp += ord(c) - ord('A') + 1
+        tmp = 8*tmp+1 # for a triangular no 8*x+1 is perfect square
+        if tmp == int(sqrt(tmp))**2:
+            cnt += 1
+    print(cnt)
