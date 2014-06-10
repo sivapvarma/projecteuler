1b483f268d8a33c742f67a38f8514fb009005e27
Siva Prasad Varma <sivapvarma@gmail.com>
2014-06-10 14:55:09 +0530

    prob 99

---
+++ python/euler99.py
@@ -0,0 +1,21 @@
+from math import log
+
+
+def main():
+    i = 1
+    res = 1
+    val = 0
+    with open('base_exp99.txt') as f:
+        for line in f:
+            b, e = line.rstrip().split(',')
+            b, e = int(b), int(e)
+            tmp = e*log(b)
+            if tmp > val:
+                res = i
+                val = tmp
+            i += 1
+    print(res)
+
+
+if __name__ == '__main__':
+    main()
