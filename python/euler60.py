c8d9ff64f1dc4965cb0509754ef4a43034763dea
Siva Prasad Varma <sivapvarma@gmail.com>
2014-06-15 18:30:10 +0530

    optimize prob 60

--- python/euler60.py
+++ python/euler60.py
@@ -1,4 +1,5 @@
 from euler import timeit
+from math import floor, log10
 
 
 is_prime = 0
@@ -43,24 +44,30 @@ def solve_op():
     lim = 10000
     prime_sieve(lim)
     print('sieve done')
-    for p1 in range(2, lim):
+    for p1 in range(3, lim, 2):
         if is_prime[p1]:
-            for p2 in range(p1+1, lim):
+            for p2 in range(p1+2, lim, 2):
                 if is_prime[p2] and prime_pair(p2, p1):
-                    for p3 in range(p2+1, lim):
+                    for p3 in range(p2+2, lim, 2):
                         if is_prime[p3] and prime_pair(p3, p1) and prime_pair(p3, p2):
-                            for p4 in range(p3+1, lim):
+                            for p4 in range(p3+2, lim, 2):
                                 if is_prime[p4] and prime_pair(p4, p1) and prime_pair(p4, p2) and prime_pair(p4, p3):
                                     print(p1, p2, p3, p4)
-                                    for p5 in range(p4+1, lim):
+                                    for p5 in range(p4+2, lim, 2):
                                         if is_prime[p5] and prime_pair(p5, p1) and prime_pair(p5, p2) and prime_pair(p5, p3) and prime_pair(p5, p4):
                                             print(p1, p2, p3, p4, p5)
                                             print('PE60 Ans: %d' % sum((p1, p2, p3, p4, p5)))
                                             return None
 
 
+# def prime_pair(n1, n2):
+#     return prime(int(str(n1)+str(n2))) and prime(int(str(n2)+str(n1)))
+
+
 def prime_pair(n1, n2):
-    return prime(int(str(n1)+str(n2))) and prime(int(str(n2)+str(n1)))
+    t1 = n1*(10**floor(log10(n2)+1)) + n2
+    t2 = n2*(10**floor(log10(n1)+1)) + n1
+    return prime(t1) and prime(t2)
 
 
 def prime(n):
