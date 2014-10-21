b149601465d3593e1fa0e34d991b5132781815be
Siva Prasad Varma <sivapvarma@gmail.com>
2014-10-21 10:24:27 +0530

    81 in Julia

---
+++ julia/euler81.jl
@@ -0,0 +1,17 @@
+A = readcsv("p081_matrix.txt")
+h, w = size(A)
+B = zeros(A)
+for i in 1:h
+    for j in 1:w
+        if i==1 && j==1
+            B[i, j] = A[i, j]
+        elseif i==1
+            B[i,j] = A[i,j] + B[i,j-1]
+        elseif j==1
+            B[i,j] = A[i,j] + B[i-1,j]
+        else
+            B[i,j] = A[i,j] + min( B[i,j-1], B[i-1,j] )
+        end
+    end
+end
+println("PE81 Ans: $(B[h,w])")
