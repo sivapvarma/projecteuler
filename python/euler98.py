8c298417acc98d0bddc9783766b5ab3fa5710fa2
Siva Prasad Varma <sivapvarma@gmail.com>
2015-02-18 11:33:59 +0530

    WIP 98 in python

---
+++ python/euler98.py
@@ -0,0 +1,6 @@
+# Question is not clear, have to experiment
+
+if __name__ == '__main__':
+    with open('../data/p098_words.txt') as f:
+        for l in f.readlines():
+            print(l.strip())
